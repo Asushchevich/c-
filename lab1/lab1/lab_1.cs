4b8beeb6865e60aab026cb820d4e1ce9c7d7fe53
Asushchevich <43595875+Asushchevich@users.noreply.github.com>
2020-12-15 11:56:52 +0300

    Add files via upload

---
+++ lab1/lab1/lab_1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            A a = new A();
+            Console.WriteLine("Введите 1 или 2\n");
+            a.p = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("c = " + a.c);
+        }
+    }
+
+    class A
+    {
+        private int a = 5;
+        private int b = 2;
+
+        public char p;
+
+        public float c
+        {
+            get
+            {
+                if (p == '1')
+                {
+                    return a * b;
+                }
+                else
+                {
+                    return a - b;
+                }
+            }
+        }
+
+        public A()
+        {
+
+        }
+    }
+}
